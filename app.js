d7e4e20940b3727909274a04e4db5591c9e289e3
sfinch6 <sfinch6@gatech.edu>
2018-04-05 16:42:14 -0400

    added logout functionality to best case senarios

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'The index page!' })
 });
 app.get('/login', function(req, res) {  
-  res.render('login', { title: 'The index page!' })
+  res.render('login', { title: 'Login page!' })
 });
 app.get('/sonika', function(req, res) {  
-  res.render('sonika', { title: 'The index page!' })
+  res.render('sonika', { title: 'Sonikas page!' })
 });
 app.get('/michael', function(req, res) {  
-  res.render('michael', { title: 'The index page!' })
+  res.render('michael', { title: 'Michaels page!' })
 });
 app.get('/tony', function(req, res) {  
-  res.render('tony', { title: 'The index page!' })
+  res.render('tony', { title: 'Tonys page!' })
 });
 app.get('/abdullah', function(req, res) {  
-  res.render('abdullah', { title: 'The index page!' })
+  res.render('abdullah', { title: 'Abdullahs page!' })
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
