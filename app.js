8b67a2781d665d9c2fbad0c6ad431defed98605a
sfinch6 <sfinch6@gatech.edu>
2018-04-05 20:17:42 -0400

    added sample video to index

--- app.js
+++ app.js
@@ -46,4 +46,35 @@ app.get('/placeNotOK', function(req, res) {
 app.get('/logout', function(req, res) {  
   res.render('logout', { title: 'Logout page!' })
 });
+app.get('/video', function(req, res) {
+  const fs = require('fs');
+  const path = 'videos/sample.mp4'
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
