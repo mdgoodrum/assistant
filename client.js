cbd9115494e41ea91b545163e4fc6513e4ab13f2
sfinch6 <sfinch6@gatech.edu>
2018-04-17 23:41:56 -0400

    added client example

---
+++ client.js
@@ -0,0 +1,20 @@
+/* Or use this example tcp client written in node.js.  (Originated with 
+example code from 
+http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
+
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(8080, 'localhost', function() {
+    console.log('Connected');
+    client.write("{ sensor1:" + sensor1 + ", sensor2:" + sensor2 + ", sensor3:" + sensor3 + "}");
+});
+
+client.on('data', function(data) {
+    console.log('Received: ' + data);
+    client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+    console.log('Connection closed');
+});
